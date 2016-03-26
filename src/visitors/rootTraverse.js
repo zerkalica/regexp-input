26bc7dbf0e8b5cca5e3f7735852f05a8ae03c93e
Stefan Zerkalica <zerkalica@gmail.com>
2016-03-26 10:27:02 +0300

    fix

--- src/visitors/rootTraverse.js
+++ src/visitors/rootTraverse.js
@@ -25,11 +25,12 @@ export type ValueState = {
 } // flow generates error
 
 type RootTraverser = (node: RegType, acc: ValueState) => void;
-
-function getMatches(node: RegType, acc: ValueState): {
+type Match = {
     normalMatch: ?Array<string>,
     strictMatch: ?Array<string>
-} {
+}
+
+function getMatches(node: RegType, acc: ValueState): Match {
     let normalMatch: ?Array<string> = null;
     let strictMatch: ?Array<string> = null;
     if (acc.current) {
@@ -46,8 +47,7 @@ function getMatches(node: RegType, acc: ValueState): {
     }
 }
 
-function baseReduce(node: RegType, acc: ValueState): boolean {
-    const {strictMatch, normalMatch} = getMatches(node, acc)
+function baseReduce({strictMatch, normalMatch}: Match, acc: ValueState): boolean {
     if (strictMatch) {
         // console.log(`strict matched ${node.raw} with ${strictMatch[0]}`)
         acc.output += acc.input
@@ -56,20 +56,21 @@ function baseReduce(node: RegType, acc: ValueState): boolean {
         // console.log(`matched ${node.raw} with ${normalMatch[0]}`)
         acc.current = acc.current.substring(normalMatch[0].length)
     } else {
-        return true
+        return false
     }
 
-    return false
+    return true
 }
 
-function valueReduce(node: RegType, acc: ValueState): void {
-    const {normalMatch} = getMatches(node, acc)
-    const inputMatch = acc.input.match(node.mask)
-    // console.log(`inputMatch: ${inputMatch ? inputMatch[0] : null}`)
-    if (inputMatch && !normalMatch) {
-        acc.output += inputMatch[0]
+function valueReduce(match: Match, node: RegType, acc: ValueState): void {
+    if (!acc.stop && !acc.current) {
+        const inputMatch = acc.input.match(node.mask)
+        // console.log(`inputMatch: ${inputMatch ? inputMatch[0] : null}`)
+        if (inputMatch && !match.normalMatch) {
+            acc.output += inputMatch[0]
+        }
+        acc.stop = true
     }
-    acc.stop = true
 }
 
 function processChilds(
@@ -87,9 +88,11 @@ function processChilds(
 }
 
 export default function rootTraverse(node: RegType, acc: ValueState): void {
+    let match: Match;
     switch (node.type) {
         case 'value':
-            if (!baseReduce(node, acc)) {
+            match = getMatches(node, acc)
+            if (!baseReduce(match, acc)) {
                 acc.output += node.raw
             }
             break
@@ -99,7 +102,8 @@ export default function rootTraverse(node: RegType, acc: ValueState): void {
         case 'disjunction':
         case 'group':
         case 'quantifier':
-            if (!baseReduce(node, acc)) {
+            match = getMatches(node, acc)
+            if (!baseReduce(match, acc)) {
                 processChilds(rootTraverse, node, acc)
             }
             break
@@ -108,67 +112,12 @@ export default function rootTraverse(node: RegType, acc: ValueState): void {
         case 'dot':
         case 'characterClassEscape':
         case 'anchor':
-            if (!acc.stop && !acc.current) {
-                valueReduce(node, acc)
-            }
+            match = getMatches(node, acc)
+            baseReduce(match, acc)
+            valueReduce(match, node, acc)
             break
         default:
             break
-    }
-
-    return
-
-    // console.log(`begin ${node.type}, ${node.raw}, acc: ${JSON.stringify(acc, 0, '  ')}`)
-    const {strictMatch, normalMatch} = getMatches(node, acc)
-
-    if (strictMatch) {
-        // console.log(`strict matched ${node.raw} with ${strictMatch[0]}`)
-        acc.output += acc.input
-        acc.stop = true
-    } else if (normalMatch) {
-        // console.log(`matched ${node.raw} with ${normalMatch[0]}`)
-        acc.current = acc.current.substring(normalMatch[0].length)
-    } else {
-        switch (node.type) {
-            case 'value':
-                acc.output += node.raw
-                break
-            case 'disjunction':
-            case 'group':
-            case 'alternative':
-            case 'quantifier': {
-                const body = node.body
-                for (let i = 0, l = body.length; i < l; i++) {
-                    rootTraverse(body[i], acc)
-                    if (acc.stop) {
-                        break
-                    }
-                }
-                break
-            }
-            default:
-                break
-        }
-    }
-
-    if (!acc.stop && !acc.current) {
-        switch (node.type) {
-            case 'characterClass':
-            case 'dot':
-            case 'characterClassEscape':
-            case 'anchor': {
-                const inputMatch = acc.input.match(node.mask)
-                // console.log(`inputMatch: ${inputMatch ? inputMatch[0] : null}`)
-                if (inputMatch && !normalMatch) {
-                    acc.output += inputMatch[0]
-                }
-                acc.stop = true
-                break
-            }
-
-            default:
-                break
-        }
     }
     // console.log(`end ${node.type}, ${node.raw}, acc: ${JSON.stringify(acc, 0, '  ')}`)
 }
